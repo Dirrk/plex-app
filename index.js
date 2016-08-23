c098240668c270ee06f28f7cab7583a45c45c7ed
Derek Rada <derekrada@gmail.com>
2016-08-22 20:19:20 -0400

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const argParser = require('minimist'),
+    app = require('./lib'),
+    config = {};
+
+config.string = ['db'];
+config.boolean = ['verbose'];
+config.default = {
+    db: 'db.json'
+};
+
+let settings = argParser(process.argv.slice(2), config);
+
+app(settings);
