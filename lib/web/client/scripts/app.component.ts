bed93aa230a62c1ae7f465ef420677a938e86f9a
rcodonnell <rcoreyodonnell@gmail.com>
2016-08-29 08:30:40 -0400

    Attempting to bootstrap angular 2

---
+++ lib/web/client/scripts/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'plex-app',
+    template: '<h1>Plex App</h1>'
+})
+export class AppComponent { }
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    entry: __dirname + '/lib/web/compiled/js/main.js',
+    output: {
+        path: __dirname + '/lib/web/public/js/',
+        filename: 'main.js'
+    }
+};
